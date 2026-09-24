4a76f22a7219a8c365bfd65e8aa374b4444ac575
yereminbrian <yereminbrianc@gmail.com>
2026-09-24 08:07:44 -0500

    Initial commit

---
+++ MatriculaBackend/src/main/java/pe/edu/upeu/MatriculaBackend/MatriculaBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.MatriculaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaBackendApplication.class, args);
+	}
+
+}
